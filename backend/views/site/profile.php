6314de6bf52189d03e711ef2b008a8b8cac6e1fe
Roman <roman@mail.casper>
2018-09-07 12:08:41 +0700

    Тест

--- backend/views/site/profile.php
+++ backend/views/site/profile.php
@@ -12,9 +12,7 @@ $this->title = 'Profile';
 $this->params[ 'breadcrumbs' ][] = $this->title;
 ?>
 <div class="site-profile">
-    <h1><?= Html::encode( $this->title ) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
+    <h3><?= Html::encode( 'Здесь можно поменять пароль' ) ?></h3>
 
     <div class="row">
         <div class="col-lg-5">
